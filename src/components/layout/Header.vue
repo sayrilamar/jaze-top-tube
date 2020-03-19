10fcf1e8c03eb77684d8bf854af2b168568c0c33
Jarius Hayes <sayrilamar@gmail.com>
2020-03-18 21:16:19 -0400

    adds searchform component

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,1 +1,29 @@
-<template><div></div> </template>
+<template>
+  <header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-5">
+      <div class="container">
+        <a class="navbar-brand" href="https://youtube.com">
+          <i class="fab fa-youtube fa-2x"></i>
+          <span class="ml-3">YouTube Search</span>
+        </a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+i {
+  vertical-align: middle;
+  color: red;
+}
+
+i + span {
+  vertical-align: middle;
+}
+</style>
